42c3302d06b0fa239545f323fd35e667286f1bd0
ljfp <ljfp@ljfp.xyz>
2023-07-17 02:28:03 -0300

    Create basic attribute set.

--- Source/Dotalike/Private/AbilitySystem/DotalikeAttributeSet.cpp
+++ Source/Dotalike/Private/AbilitySystem/DotalikeAttributeSet.cpp
@@ -2,4 +2,43 @@
 
 
 #include "AbilitySystem/DotalikeAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UDotalikeAttributeSet::UDotalikeAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(100.f);
+	InitMaxMana(100.f);
+}
+
+void UDotalikeAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Register attributes for replication on the clients
+	DOREPLIFETIME_CONDITION_NOTIFY(UDotalikeAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDotalikeAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDotalikeAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDotalikeAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UDotalikeAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDotalikeAttributeSet, Health, OldHealth);
+}
+
+void UDotalikeAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDotalikeAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UDotalikeAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDotalikeAttributeSet, Mana, OldMana);
+}
+
+void UDotalikeAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDotalikeAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/Dotalike/Public/AbilitySystem/DotalikeAttributeSet.h
+++ Source/Dotalike/Public/AbilitySystem/DotalikeAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "DotalikeAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +20,35 @@ UCLASS()
 class DOTALIKE_API UDotalikeAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	UDotalikeAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UDotalikeAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UDotalikeAttributeSet, MaxHealth);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Magical Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UDotalikeAttributeSet, Mana);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Magical Attributes")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UDotalikeAttributeSet, MaxMana);
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
